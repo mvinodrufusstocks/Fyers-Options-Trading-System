21fd7cfafc2f1f6768286e6a6952ad5df993639d
mvinodrufusstocks <m.vinodrufus.stocks@gmail.com>
2025-08-01 19:32:50 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 
 export default function TradingDashboard() {
